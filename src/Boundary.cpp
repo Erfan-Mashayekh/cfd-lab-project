68320498a8e2e44b6268a741d0d30cfc25e188f4
Binu Amaratunga <ge75rud@mytum.de>
2021-05-20 23:54:27 +0530

    Merge boundary conditions

--- src/Boundary.cpp
+++ src/Boundary.cpp
@@ -40,9 +40,47 @@ FreeSlipBoundary::FreeSlipBoundary(std::vector<Cell *> cells, std::map<int, doub
 *
 **************************************/
 
-void InflowBoundary::apply(Fields &field){}
+void InflowBoundary::apply(Fields &field){
 
-void OutflowBoundary::apply(Fields &field){}
+    for (auto const& cell: _cells) {
+        
+        // Set inlet velocities
+        field.u(cell->i(), cell->j()) = _inlet_velocity_x;
+        
+        field.v(cell->i(), cell->j()) = _inlet_velocity_y;
+
+        // Set F and G
+        field.f(cell->i(), cell->j()) = field.u(cell->i(), cell->j());
+
+        field.g(cell->i(), cell->j()) = field.v(cell->i(), cell->j());
+
+        // TODO: Set inlet temperature
+
+    }
+}
+
+void OutflowBoundary::apply(Fields &field){
+
+    for (auto const& cell : _cells) {
+
+        // Assume single fluid cell neighbour
+        std::vector<border_position> border_pos = cell->borders();
+
+        if(border_pos.size() != 1){
+            std::cout << "Warning! Outflow can have only single fluid neighbour cells!" << std::endl;
+        }
+
+        // Set inlet velocities (Neumann boundary)
+        field.u(cell->i(), cell->j()) = field.u(cell->neighbour(border_pos.at(0))->i(), cell->neighbour(border_pos.at(0))->j());
+        
+        field.v(cell->i(), cell->j()) = field.v(cell->neighbour(border_pos.at(0))->i(), cell->neighbour(border_pos.at(0))->j());
+
+        // Set F and G
+        field.f(cell->i(), cell->j()) = field.u(cell->i(), cell->j());
+
+        field.g(cell->i(), cell->j()) = field.v(cell->i(), cell->j());
+    }
+}
 
 void FixedWallBoundary::apply(Fields &field){
     /** 
@@ -55,41 +93,44 @@ void FixedWallBoundary::apply(Fields &field){
      * averaging the values on both sides of the boundary. Thus Vout = -vin.
      * 
      */
-    
-    // Applying u,v and F boundary conditions on every border
-    /*
-    Here we have something strange happening at the code. When we try to ensure the border positions at the right, it
-    shows the left and so otherwise (Also the TOP and Bottom). 
-    Hence, here we using the border position that is correct according to the i() andj() that is reversed, in order to get the correct border.
 
-    */
+    for(auto const& cell: _cells){
+        
+        std::vector<border_position> border_positions = cell->borders();
 
-    // Loop through all cells an applying boundary conditions if the cells in located at the border.
-    for(auto cell: _cells){
-        // Fix code duplication
-        if(cell->is_border(border_position::RIGHT)){
-            // u on the left
-            field.u(cell->i(), cell->j()) = 0;
-            // v on left 
-            field.v(cell->i(), cell->j()) = - field.v(cell->neighbour(border_position::RIGHT)->i(), cell->j());
-            // F on the left 
-            field.f(cell->i(), cell->j()) = field.u(cell->i(), cell->j());
-        }
-        else if(cell->is_border(border_position::LEFT)){
-            // u on the right
-            field.u(cell->i(), cell->j()) = 0;
-            // v  on right 
-            field.v(cell->i(), cell->j()) = - field.v(cell->neighbour(border_position::LEFT)->i(), cell->j());
-            // F on the right
-            field.f(cell->i(), cell->j()) = field.u(cell->i(), cell->j());
-        }
-        else if(cell->is_border(border_position::TOP)){
+        // Set velocity to zero at inner obstacle cells
+        if(border_positions.size() == 0){
             // u 
-            field.u(cell->i(), cell->j()) = - field.u(cell->i(), cell->neighbour(border_position::TOP)->j());
+            field.u(cell->i(), cell->j()) = 0;
             // v 
-            field.v(cell->i(), cell->j()) = 0;    
-            // g
-            field.g(cell->i(), cell->j()) = field.v(cell->i(), cell->j()); 
+            field.v(cell->i(), cell->j()) = 0;
+            // F 
+            field.f(cell->i(), cell->j()) = 0;
+            // G
+            field.g(cell->i(), cell->j()) = 0;
+
+            continue;
+        }
+
+        for(auto const& border_pos: border_positions){
+
+            if(cell->is_border(border_position::RIGHT) || cell->is_border(border_position::LEFT)){
+                // u 
+                field.u(cell->i(), cell->j()) = 0;
+                // v 
+                field.v(cell->i(), cell->j()) = - field.v(cell->neighbour(border_pos)->i(), cell->j());
+                // F 
+                field.f(cell->i(), cell->j()) = field.u(cell->i(), cell->j());
+            }
+
+            if(cell->is_border(border_position::TOP) || cell->is_border(border_position::BOTTOM)){
+                // u 
+                field.u(cell->i(), cell->j()) = - field.u(cell->i(), cell->neighbour(border_pos)->j());
+                // v 
+                field.v(cell->i(), cell->j()) = 0;    
+                // G
+                field.g(cell->i(), cell->j()) = field.v(cell->i(), cell->j());
+            }
         }
     }
 }
@@ -99,20 +140,29 @@ void MovingWallBoundary::apply(Fields &field) {
 
     /* The top wall is a moving wall thus, the velocity on the top wall would be the velocity of the wall itself.*/
 
-    // Same aboveLoop through all cells an applying boundary conditions if the cells in located at the border.
-    // However this here, we have a moving wall so the wall velocity is accounted for.
-
     for(auto cell: _cells){
-        if(cell->is_border(border_position::BOTTOM) && cell->type() == cell_type::MOVING_WALL){
-            // v on the top
-            field.v(cell->i(), cell->j()) = 0;
-            // u on the top 
-            field.u(cell->i(), cell->j()) = 2.0 * _wall_velocity[cell->wall_id()] - field.u(cell->i(), cell->neighbour(border_position::BOTTOM)->j());
-            // F on the left 
-            field.g(cell->i(), cell->j()) = field.v(cell->i(), cell->j());
+
+        std::vector<border_position> border_pos = cell->borders();
+
+        // Skip the two cells at the corners
+        if(border_pos.size() == 0){
+            continue;
         }
+
+        if(border_pos.size() > 1){
+            std::cout << "Warning! Moving wall can have only single fluid neighbour cells!" << std::endl;
+        }
+
+        // v on the top
+        field.v(cell->i(), cell->j()) = 0;
+        // u on the top 
+        field.u(cell->i(), cell->j()) = 2.0 * _wall_velocity[cell->wall_id()] - field.u(cell->i(), cell->neighbour(border_pos.at(0))->j());
+        // F on the left 
+        field.g(cell->i(), cell->j()) = field.v(cell->i(), cell->j());
     }
 }
 
 
-void FreeSlipBoundary::apply(Fields &field) {}
+void FreeSlipBoundary::apply(Fields &field) {
+    // Implement
+}
